59a05a3900bddcc329307a099ba5ae2861661b35
1perfectPFW <94060781+1perfectPFW@users.noreply.github.com>
2021-11-10 22:59:35 +0800

    Add files via upload
    0xbdfab42a028770f42a37458dc1bfe9531158826d

---
+++ perfect.sol
@@ -0,0 +1,25 @@
+CMC APPLICATION FORM
+
+Coin/token name: Perfect World
+Bsc: https://bscscan.com/token/0xbdfab42a028770f42a37458dc1bfe9531158826d
+Symbol: PFW
+Contract address: 0xbdfab42a028770f42a37458dc1bfe9531158826d
+Reddit:
+Twitter: https://twitter.com/BSc_PFW
+Facebook:
+Source Code: https://bscscan.com/address/0xbdfab42a028770f42a37458dc1bfe9531158826d#code
+Github:
+Website: https://pfw.ac/
+Country of origin: USA
+Total supply: 900000000000000
+Max supply: 9000000000000000
+Telegram: https://t.me/PerfectWorld666
+Discord;
+Logo link 200x200 tansparent background: https://www.dreammanor.net/Manor_files/logo.png
+
+Brief Description about token, should be appealing:
+A new decentralized meta-universe ecosystem including digital asset trading platform, P2E online finance, NFT sector, GAMEFI, etc. All digital
+platforms-mobile, computers, consoles and VR/AR can sell virtual game props by executing smart contracts with blockchain technology and removingtrusted intermediaries. Game item trading will use encrypted token "PFW"
+Meta universe is a new type of Internet application and social form that integrates multiple new technologies. It provides an immersive experiencebased on extended reality technology, generates a mirror image of the real world based on digital twin technology, and builds an economic systembased on blockchain technology. , The virtual world and the real world are closely integrated in the economic system, social system, and identitysystem, and each user is allowed to carry out content production and world editing.
+Exchange (s) trading link:
+https://pancakeswap. finance/swap?inputCurrency BNB&outputCurrency=0xbdfab42a028770f42a37458dc1bfe9531158826d
